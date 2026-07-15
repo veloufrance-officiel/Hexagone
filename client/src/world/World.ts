60ec121a4b94b2f1e22a2a9528c8e0d4fac1e01a
veloufrance-officiel <veloufrance@gmail.com>
2026-07-15 19:13:45 +0200

    Update World.ts

--- client/src/world/World.ts
+++ client/src/world/World.ts
@@ -1,1 +1,39 @@
+import { ChunkManager } from "./ChunkManager";
+import { StreamingController } from "./StreamingController";
+import { WorldRegistry } from "./WorldRegistry";
 
+export class World {
+  public readonly registry: WorldRegistry;
+  public readonly chunkManager: ChunkManager;
+  public readonly streamingController: StreamingController;
+
+  constructor() {
+    this.registry = new WorldRegistry();
+
+    this.chunkManager = new ChunkManager();
+
+    this.streamingController =
+      new StreamingController(
+        this.chunkManager
+      );
+  }
+
+  public load(): void {
+    console.log("HEXAGONE World initialized");
+  }
+
+  public update(
+    playerX: number,
+    playerZ: number
+  ): void {
+    this.streamingController.update(
+      playerX,
+      playerZ
+    );
+  }
+
+  public dispose(): void {
+    this.chunkManager.clear();
+    this.registry.clear();
+  }
+}
