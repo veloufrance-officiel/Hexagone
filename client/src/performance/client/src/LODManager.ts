20bd3a4d0cb5d5c65e0679bb37b7ee2ff1b8dc40
veloufrance-officiel <veloufrance@gmail.com>
2026-07-15 18:43:57 +0200

    Create LODManager.ts

---
+++ client/src/performance/client/src/LODManager.ts
@@ -0,0 +1,21 @@
+LODManager
+
+registerObject()
+        |
+        |
+      LODObject
+        |
+        +── mesh haute qualité
+        +── mesh moyenne qualité
+        +── mesh basse qualité
+
+
+update(playerPosition)
+
+        ↓
+
+distance calculée
+
+        ↓
+
+choix LOD
